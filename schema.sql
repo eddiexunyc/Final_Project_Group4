a31fd811ccb78527d7a4db7013f72095c69c77fe
Vasudha Nair <69016432+vasudhanair@users.noreply.github.com>
2021-01-28 13:39:05 -0500

    Schema showing  all the tables

--- schema.sql
+++ schema.sql
@@ -35,3 +35,27 @@ CREATE TABLE evsalesyearly (
     year_2018 int,
     year_2019 int
 );
+
+CREATE TABLE teslasales (
+	tesla_vehicle text,
+	tech_type text,
+	year_2011 int,
+    year_2012 int,
+	year_2013 int,
+	year_2014 int,
+	year_2015 int,
+    year_2016 int,
+    year_2017 int,
+    year_2018 int,
+    year_2019 int,
+    total int
+);
+
+CREATE TABLE teslaquarterlysale (
+	year_teslasale int,
+	q1 int,
+	q2 int,
+	q3 int,
+	q4 int,
+	yearly_total int
+);
